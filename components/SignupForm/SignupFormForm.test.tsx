57526dc6a02b4b5c9491f5a82371d8071404a15e
Ruslan Posevkin <rus.posevkin@gmail.com>
2024-05-13 17:09:42 +0100

    Revert "Add newsletter-signup page and form component"
    This reverts commit 9e68d4c69ade5935a1ad5ce68429458492599a14.

--- components/SignupForm/SignupFormForm.test.tsx
+++
@@ -1,9 +0,0 @@
-import {render, screen} from '@testing-library/react'
-import '@testing-library/jest-dom'
-import SignupForm from '.'
-
-test('it should match the snapshot and render', async () => {
-  const { container } = render(<SignupForm />);
-  expect(container).toMatchSnapshot();
-  expect(container).toBeInTheDocument();
-});

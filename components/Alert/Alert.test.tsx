f051b3845e86c00f69bb174f2f82b59128c8a8ba
Ruslan Posevkin <rus.posevkin@gmail.com>
2024-05-13 21:24:35 +0100

    Fix Alert test

--- components/Alert/Alert.test.tsx
+++ components/Alert/Alert.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react'
+import { act, render, screen, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
@@ -32,10 +32,12 @@ test('it should show a message after updating the store', async () => {
     )
   );
 
-  store.dispatch(setNewsletterState({
-    message: 'test message',
-    isSubmitted: true
-  }));
+  act(() => {
+    store.dispatch(setNewsletterState({
+      message: 'test message',
+      isSubmitted: true
+    }))
+  });
 
   await waitFor(() => {
     expect(screen.getByText('test message')).toBeInTheDocument();
